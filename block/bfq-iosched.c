78e7c3a62dc853804cc071cc29eda9ec1e523950
Paolo Valente <paolo.valente@linaro.org>
2016-07-13 20:01:32 +0200

    Tell whether cgroups support is enabled at boot
    Suggested-by: Lee Tibbert <lee.tibbert@gmail.com>
    Signed-off-by: Albert I <kras@raphielgang.org>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -4797,6 +4797,7 @@ static struct blkcg_policy blkcg_policy_bfq = {
 static int __init bfq_init(void)
 {
 	int ret;
+	char msg[50] = "BFQ I/O-scheduler: v8-rc3";
 
 	/*
 	 * Can be 0 on HZ < 1000 setups.
@@ -4843,7 +4844,10 @@ static int __init bfq_init(void)
 	if (ret)
 		goto err_pol_unreg;
 
-	pr_info("BFQ I/O-scheduler: v8-rc3");
+#ifdef CONFIG_BFQ_GROUP_IOSCHED
+	strcat(msg, " (with cgroups support)");
+#endif
+	pr_info("%s", msg);
 
 	return 0;
 
